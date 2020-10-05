36f02012f321a1d787492bf4c7ede037896b8cfc
Anatoliy <nikzanddemon@mail.ru>
2020-10-05 16:11:39 +0500

    перешел с Thread.sleep() на TimerTask()

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -7,15 +7,14 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import sun.util.calendar.BaseCalendar;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Parser {
 
@@ -25,93 +24,87 @@ public class Parser {
         Document page = Jsoup.parse(new URL(url), 3000);
         return page;
     }
-    private static double getN( Element a) throws IOException {
+    private static double getN( Element a) {
         String b = a.text();
         b = b.substring( b.indexOf(" "), b.lastIndexOf(" "));
         b=b.trim();
         int N = Integer.parseInt( b );
         return N;
     }
-   /* private static void getPeepls (Element a) {
-        String b =a.text();
-        System.out.println(b);
-        Pattern pattern= Pattern.compile("\\s");
-        Matcher matcher = pattern.matcher(b);
-        System.out.println(matcher);
-        System.out.println();
-    }
-*/
+
 
     public static void main(String[] args) throws IOException {
 
+        final Timer timer=new Timer();
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                Calendar date= Calendar.getInstance();
+                int d = date.get(Calendar.DAY_OF_MONTH);
+                int h = date.get(Calendar.HOUR_OF_DAY);
+                int m = date.get(Calendar.MINUTE);
 
-         while (true) {
-             label1:
-             {
-                 Calendar call= Calendar.getInstance();
-                 Date today = call.getTime();
-                 int moon = today.getMonth();
-                 int d = today.getDate();
-                 int h = today.getHours();
-                 int m = today.getMinutes();
+                if (h<8) {
+                    timer.cancel();
+                } else {
 
-                 //System.out.println(today.toString());
-                 System.out.println(h+":"+m);
-
-                 if (h < 8) {
-                     long l = ((7 - h) * 60 + (63 - m)) * 60000;
-                     try {
-                         Thread.sleep(l); // Замораживает весь поток на 10 секунд
-                         break label1;
-                     } catch (Exception e) {
-                         System.out.println("Получили исключение по времени!");
-                     }
-
-                 }
+                    try {
 
 
-                 Document page = getPage();
-                 Elements span = page.select("span");
-                 Element h2 = span.get(1);
-                 double N = getN(h2);
+                        Document page = getPage();
+                        Elements span = page.select("span");
+                        Element h2 = span.get(1);
+                        System.out.println(h2);
+                        double N = getN(h2);
+
+                        FileInputStream fis = new FileInputStream("ststistica.xls");
+                        Workbook wb = new HSSFWorkbook(fis);
+                        fis.close();
 
 
-                 System.out.println(h2);
+                        Sheet sh1 = wb.getSheetAt(0);
+                        Row r1 = sh1.getRow(d);
+                        System.out.println("Строка " + d);
 
-                 FileInputStream fis = new FileInputStream("C:/Users/Lenovo/IdeaProjects/traffic/ststistica.xls");
-                 Workbook wb = new HSSFWorkbook(fis);
-                 fis.close();
+                        try {
+
+                            Cell cell = r1.createCell(((h - 8) * 4) + (m / 15) + 1);
+                            cell.setCellValue(N);
+                            System.out.println("Значение " + N + " записано в " + (((h - 8) * 4) + (m / 15) + 1) + " столбик");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            System.out.println("Не удалось создать ячейку");
+                        }
 
 
-                 Sheet sh1 = wb.getSheetAt(0);
-                 Row r1 = sh1.getRow(d);
-                 System.out.println("Строка " + d);
-
-                try {
-
-                    Cell cell = r1.createCell(((h - 8) * 4) + (m / 15)+1);
-                    cell.setCellValue(N);
-                    System.out.println( "Значение " + N + " записано в " + (((h - 8) * 4) + (m / 15)+1) + " столбик" );
-                } catch (Exception e) {
-                    System.out.println("Не удалось создать ячейку");
+                        FileOutputStream fos = new FileOutputStream("C:/Users/Lenovo/IdeaProjects/traffic/ststistica.xls");
+                        wb.write(fos);
+                        fos.close();
+                        System.out.println("Таблица сохранена");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        System.out.println("Что-то пошло не так");
+                        timer.cancel();
+                    }
                 }
 
+            }
+        };
 
-                 FileOutputStream fos = new FileOutputStream("C:/Users/Lenovo/IdeaProjects/traffic/ststistica.xls");
-                 wb.write(fos);
-                 fos.close();
-                 System.out.println("Таблица сохранена");
+        Calendar call= Calendar.getInstance();
+        int h = call.get(Calendar.HOUR_OF_DAY);
+        int m = call.get(Calendar.MINUTE);
+        System.out.println("Сейчас "+call.getTime().toString());
 
-                 try {
-                     Thread.sleep(900000-2500); // Замораживает весь поток на
-                 } catch (Exception e) {
-                     System.out.println("Получили исключение по времени!");
-                 }
-             }
-         }
+        if (h<8) {call.set(Calendar.HOUR_OF_DAY, 8);
+        }
+        if (m%15>5) {call.add(Calendar.MINUTE, (16-m%15));
+        } else { call.add(Calendar.SECOND, 10);
+        }
 
+        System.out.println("Время первого опроса"+call.getTime().toString());
 
-
+        timer.scheduleAtFixedRate(task, call.getTime(), 900000);
 
 
 
